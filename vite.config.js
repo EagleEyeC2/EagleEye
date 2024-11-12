49e1b2e5a48959dcaec4b3a2890725eab880a0cc
visions0224 <visions0224@gmail.com>
2024-11-12 19:18:49 +0800

    updata ymlfile

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EagleEye/', // 添加你的仓库名称
+  base: '/EagleEye/', // GitHub Pages 的仓库名称
 })
